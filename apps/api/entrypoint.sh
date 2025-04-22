962e925ee39c41eb4cb428bd93b7f08b0a1fbb01
nicolas Audeon <dev-nau@krysinfo.fr>
2025-04-22 14:33:06 +0200

    Chore (api) : correct entrypoint.sh

--- apps/api/entrypoint.sh
+++ apps/api/entrypoint.sh
@@ -2,7 +2,7 @@
 export BREVO_API_KEY=$(cat /run/secrets/brevo_api_key)
 export DB_HOST=mysql
 export DB_PORT=3306
-export DB_NAME: mysmartfamily
+export DB_NAME=mysmartfamily
 export DB_USER=mysmartfamily
 export DB_PASSWORD=$(cat /run/secrets/mysql_user_password)
 export FILE_STORAGE_URL=http://dufs:5000
