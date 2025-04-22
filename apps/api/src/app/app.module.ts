5bbc27a9915335efcb8f75ec703e208765e73044
nicolas Audeon <dev-nau@krysinfo.fr>
2025-04-22 15:12:24 +0200

    Chore (api) : valide env

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { DatasModule } from '@mysmartfamily/backend-datas';
 import { DocumentsModule } from '@mysmartfamily/backend-documents';
 import { EmailsModule } from '@mysmartfamily/backend-emails';
 import { ConfigModule } from '@nestjs/config';
+import { validate } from './env.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validate,
     }),
     DatasModule,
     DocumentsModule,
