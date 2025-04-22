da7c5593ffb52be6d5c12522d4254d330d221989
nicolas Audeon <dev-nau@krysinfo.fr>
2025-04-22 16:45:50 +0200

    build : correct db user and password

--- apps/api/entrypoint.sh
+++ apps/api/entrypoint.sh
@@ -3,8 +3,8 @@ export BREVO_API_KEY=$(cat /run/secrets/brevo_api_key)
 export DB_HOST=mysql
 export DB_PORT=3306
 export DB_NAME=mysmartfamily
-export DB_USER=mysmartfamily
-export DB_PASSWORD=$(cat /run/secrets/mysql_user_password)
+export DB_USER=root
+export DB_PASSWORD=$(cat /run/secrets/mysql_root_password)
 export FILE_STORAGE_URL=http://dufs:5000
 
 node dist/main.js
